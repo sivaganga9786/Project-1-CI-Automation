784ee2c39745ce98b5dd97cf88cf196319b840eb
siva <siva@example.com>
2025-08-10 20:57:26 +0530

    change the bucket name

--- terraform-Modules/Dev/backend.tf
+++ terraform-Modules/Dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-dev" # Will be created by module
+    bucket = "terraform-state-dev-ecom" # Will be created by module
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
--- terraform-Modules/Dev/main.tf
+++ terraform-Modules/Dev/main.tf
@@ -1,6 +1,6 @@
 module "s3_backend" {
   source      = "git::https://github.com/sivaganga9786/Terraform-foundation.git//terraform-resources/S3"
-  bucket_name = "terraform-state-dev"
+  bucket_name = "terraform-state-dev-ecom"
   environment = "dev"
   tags = {
     Project = "terraform-Ecom"
